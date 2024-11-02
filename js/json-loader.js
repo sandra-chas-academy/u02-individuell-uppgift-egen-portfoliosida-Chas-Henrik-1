c0b2d6566baf1b51c93d224917b614cec0fb9528
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-11-02 16:08:36 +0100

    Added 24h timestamp expiration

--- js/json-loader.js
+++ js/json-loader.js
@@ -1,14 +1,24 @@
 export async function loadJSONData(url) {
     try {
+        const oneDay = 86400000; // 24 hours in milliseconds
         let cvDataObj = JSON.parse(localStorage.getItem("cvDataObj"));
 
-        if(cvDataObj == null){
+        // If timestamp is missing or the timestamp is older than 24 hours, fetch the data again
+        if(cvDataObj == null || cvDataObj.timeStamp == null || Date.now() > parseInt(cvDataObj.timeStamp) + oneDay) {
             const response = await fetch(url);
             cvDataObj = await response.json();
-            localStorage.setItem("cvDataObj", JSON.stringify(cvDataObj));
+            saveToLocalStorage(cvDataObj);
         }
+
         return cvDataObj;
     } catch (error) {
         console.error("Error:", error);
     }
+}
+
+function saveToLocalStorage(cvDataObj) {
+    // Add a new timestamp to the object
+    cvDataObj.timeStamp =  Date.now();
+    console.log("cvDataObj.timeStamp", cvDataObj.timeStamp);
+    localStorage.setItem("cvDataObj", JSON.stringify(cvDataObj));
 }
